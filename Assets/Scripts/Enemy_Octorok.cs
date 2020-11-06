eb3b14fa9a3248354ae0073f6bc408f76b8590ec
Drew Caines <drewcaines@gmail.com>
2020-11-05 19:10:39 -0500

    started octorok scripting

---
+++ Assets/Scripts/Enemy_Octorok.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//PURPOSE: Movement code for the Octoroks (rock shooty octopi)
+//USAGE: Attached to Octorok Prefab
+public class Enemy_Octorok : MonoBehaviour
+{
+    public float speed;
+    int octorokHealth;
+    public bool strongVersion;
+    Enemy_HP myHP;
+    public float counter;
+    public float max_counter;
+    public bool straight;
+    public float timeStraight;
+    public float max_timeStraight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
