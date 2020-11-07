3131b304bab287386bc485c26bbb39a9cfc906c8
Eli <esc487@nyu.edu>
2020-11-06 21:00:24 -0600

    TILEMAPPING
    Using Eris' environmental sprites (thank you!), I created a tilemap of most of what we will be working on
    The tilemap has colliders, but it might need some work to make it work with player movement.
    Got the Zora (water enemy) work with going underwater/resurfacing at a different location - still got to work on the animation part, and that enemies fireballs
    Camera movement needs to be reworked to work with the stat bar at the top of the screen.

--- Assets/Scripts/Enemy_Zora.cs
+++ Assets/Scripts/Enemy_Zora.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 //PURPOSE: Code for the water enemies that shoot at the player
 //USAGE: Attached to a Zora Prefab
@@ -28,10 +29,41 @@ public class Enemy_Zora : MonoBehaviour
     public float max_shotDelay;
     //Whether or not the enemy needs to shoot a projectile
     bool shoot = false;
+    public Camera myCamera;
+    public int statBarOffest;//used for the stat bar at the top of the screen - need to find a better way to do this
+    //These Tilemaps below are used to determine where this object can spawn
+    public Tilemap myTilemap;
+    public List<Vector3> waterTiles  = new List<Vector3>();
+    public List<Sprite> possibleWaterTiles = new List<Sprite>();//List of possible water sprites this creature can spawn on
     void Start()
     {
         myHP = GetComponent<Enemy_HP>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();   
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        //get a list of tiles this guy can spawn on
+        //For loop of y values, than x values
+        for(int i = 0; i < myCamera.orthographicSize * 2 - statBarOffest; i++) {
+            for(int j = 0; j < myCamera.orthographicSize * 2 * myCamera.aspect; j++ ) {
+                //x position is equal to bottom left corner plus j
+                int x = (int)(myCamera.transform.position.x - myCamera.orthographicSize * myCamera.aspect) + j; 
+                //y position is equal to bottom left corner plus i
+                int y = (int)(myCamera.transform.position.y - myCamera.orthographicSize) + i;
+                //Check the tile at that position
+                Tile myTile = myTilemap.GetTile<Tile>(new Vector3Int(x, y, 0));
+                if(myTile != null) {
+                    for(int k = 0; k< possibleWaterTiles.Count; k++) {
+                        if(myTile.sprite == possibleWaterTiles[k]) {
+                            Debug.Log(x.ToString() + " " + y.ToString());
+                            waterTiles.Add(new Vector3((float)x + 0.5f, (float)y +0.5f, 0f));
+                        } 
+                    }
+                } 
+            }
+        }
+        //If there is at least 1 water tile, this enemy is teleported to 1 of them when it spawns
+        if(waterTiles.Count > 0) {
+            int rnd = Random.Range(0, waterTiles.Count - 1);
+            transform.position = waterTiles[rnd];
+        }
     }
 
     // Update is called once per frame
@@ -71,6 +103,8 @@ public class Enemy_Zora : MonoBehaviour
         if(diveTime > max_diveTime / 2 && !positionReset) {
             positionReset = true;
             //Gonna probably use tilemapping for this
+            int rnd = Random.Range(0, waterTiles.Count - 1);
+            transform.position = waterTiles[rnd];
         }
     }
 }
