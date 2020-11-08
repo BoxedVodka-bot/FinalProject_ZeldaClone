d72c101506faf35e1d09f902289302994d46f1ff
BoxedVodka <icecompass.e.s@gmail.com>
2020-11-08 18:24:06 -0500

    Inventory(basic A&B) and Collecting

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -11,8 +12,17 @@ public class PlayerControl : MonoBehaviour
     public Animator anim;
     //public Vector2 movement;
     private bool isWalking;
-    private float x, y;
+    public float x, y;
 
+    //Collecting objects
+    public int diamond = 0;
+    public int key = 0;
+    public int orb = 0;
+
+    //Number counting
+    public Text diamondNum;
+    public Text keyNum;
+    public Text orbNum;
 
    
     void Start()
@@ -20,7 +30,7 @@ public class PlayerControl : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //myAudioSource = GetComponent<AudioSource>();
     }
-     void Update()
+    void Update()
     {
         x = Input.GetAxisRaw("Horizontal"); //Gets a value from -1 to 1. -1 if left, 1 if right.
         y = Input.GetAxisRaw("Vertical");
@@ -47,5 +57,21 @@ public class PlayerControl : MonoBehaviour
         transform.Translate(x*Time.deltaTime*moveSpeed, y*Time.deltaTime*moveSpeed, 0);
     }
     
-
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == "Diamond"){
+            Destroy(collision.gameObject);
+            diamond += 1;
+            diamondNum.text = diamond.ToString();
+        }
+        if (collision.tag == "Key"){
+            Destroy(collision.gameObject);
+            key += 1;
+            keyNum.text = key.ToString();
+        }
+        if (collision.tag == "Orb"){
+            Destroy(collision.gameObject);
+            orb += 1;
+            orbNum.text = orb.ToString();
+        }
+    }
 }
