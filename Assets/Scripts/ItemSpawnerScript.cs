2420adb79ae91080a18c524b8ec5af9ac08dd46e
Dominik Kopiczko <dominik.kopiczko@gmail.com>
2020-11-15 15:20:26 -0500

    Added NPC assets and finished up NPC spawning for five NPC rooms

--- Assets/Scripts/ItemSpawnerScript.cs
+++ Assets/Scripts/ItemSpawnerScript.cs
@@ -7,6 +7,11 @@ public class ItemSpawnerScript : MonoBehaviour
     public int ItemSequenceNumber;
     public int item;
     public GameObject Sword;
+    public GameObject Shield;
+    public GameObject Key;
+    public GameObject Candle;
+    public GameObject Bomb;
+    public GameObject Arrow;
     public bool ItemsSpawned;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +25,27 @@ public class ItemSpawnerScript : MonoBehaviour
     {
         // if the item sequence number is 1
         if (ItemSequenceNumber == 1 && ItemsSpawned == false){
-            Instantiate(Sword, new Vector3 (0, -2, 0), Quaternion.identity);
+            Instantiate(Sword, new Vector3 (0, -0.75f, 0), Quaternion.identity);
+            ItemsSpawned = true;
+        }
+        if (ItemSequenceNumber == 2 && ItemsSpawned == false){
+            Instantiate(Shield, new Vector3 (-2, -0.75f, 0), Quaternion.identity);
+            Instantiate(Key, new Vector3 (0, -0.75f, 0), Quaternion.identity);
+            Instantiate(Candle, new Vector3 (2, -0.75f, 0), Quaternion.identity);
+            ItemsSpawned = true;
+            // if you have x rupees, you can buy something
+        }
+        if (ItemSequenceNumber == 3 && ItemsSpawned == false){
+            Instantiate(Shield, new Vector3 (-2, -0.75f, 0), Quaternion.identity);
+            Instantiate(Bomb, new Vector3 (0, -0.75f, 0), Quaternion.identity);
+            Instantiate(Arrow, new Vector3 (2, -0.75f, 0), Quaternion.identity);
+            ItemsSpawned = true;
+            // if you have x rupees, you can buy something
+        }
+        if (ItemSequenceNumber == 4 && ItemsSpawned == false){
+            ItemsSpawned = true;
+        }
+        if (ItemSequenceNumber == 5 && ItemsSpawned == false){
             ItemsSpawned = true;
         }
     }
--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -7,22 +7,45 @@ public class SceneManagerScript : MonoBehaviour
     static int RoomNumber;
     public NPCSpawnerScript NPCSpawn;
     public ItemSpawnerScript ItemSpawn;
+    public GameObject PlayerObject;
+    public bool PlayerSpawned;
 
     // Start is called before the first frame update
     void Start()
     {
         // eventually create a list of roomnumbers here
-        RoomNumber = 1;
+        RoomNumber = 4;
+        PlayerSpawned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PlayerSpawned == false){
+            Instantiate(PlayerObject, new Vector3 (0, -5.5f, 0), Quaternion.identity);
+            PlayerSpawned = true;
+        }
         // if room number = x , spawn the corresponding x npc and items
         // if you have the sword, don't spawn the sword again
         if (RoomNumber == 1){
             NPCSpawn.NPCNumber = 1;
             ItemSpawn.ItemSequenceNumber = 1;
+        }
+        if (RoomNumber == 2){
+            NPCSpawn.NPCNumber = 2;
+            ItemSpawn.ItemSequenceNumber = 2;
+        }
+        if (RoomNumber == 3){
+            NPCSpawn.NPCNumber = 3;
+            ItemSpawn.ItemSequenceNumber = 3;
+        }
+        if (RoomNumber == 4){
+            NPCSpawn.NPCNumber = 4;
+            ItemSpawn.ItemSequenceNumber = 4;
+        }
+        if (RoomNumber == 5){
+            NPCSpawn.NPCNumber = 5;
+            ItemSpawn.ItemSequenceNumber = 5;
         }
     }
 }
